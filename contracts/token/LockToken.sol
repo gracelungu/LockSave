de02e863893f027e33a8491660bcbd7260503138
gracelbw <grace.lungu.bw@gmail.com>
2021-11-29 20:04:28 +0200

    Add crowdsales

--- contracts/token/LockToken.sol
+++ contracts/token/LockToken.sol
@@ -5,15 +5,15 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LockToken is ERC20 {
-    address private owner;
+    address private admin;
 
     constructor() ERC20("Lock", "LOCK") {
         _mint(msg.sender, 1000000000 * 10 ** 18);
-        owner = msg.sender;
+        admin = msg.sender;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == admin);
         _;
     }
 
@@ -23,5 +23,9 @@ contract LockToken is ERC20 {
 
     function destroy(uint256 _amount) public onlyOwner {
         _burn(msg.sender, _amount);
+    }
+
+    function crowdSales(address _to, uint256 _amount) public payable onlyOwner {
+        _mint(_to, _amount);
     }
 }
